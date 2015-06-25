737896cc13f1b8d0becb009186f69681e902d7f8
Sophia-Marie <soffi-marie@hotmail.com>
2015-06-25 11:46:46 +0200

    functions added

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import pygame
 
+def draw_Field(colour):
+    colour= colour
+    positionList = [100, 100, 70, 70]
+    for position in range(1,65):
+        pygame.draw.rect(window, colour, positionList)
+        if position !=0 and position %8==0:
+            positionList[1] +=72
+            positionList[0] = 100
+        else :
+            positionList[0] +=72
+
+
+def statusColour():
+    statusRect= pygame.mouse.get_pressed()
+    statusColour=(0,0,0)
+    
+    if statusRect==True:
+        statusColour= PINK
+        
+    else :
+        statusColour= GREEN
+
+    return statusColour
+
 pygame.init()
 
 window = pygame.display.set_mode((800,700))
 
 pygame.display.set_caption("Reversi")
 
-#Farben
+#Colours
+PINK  =  (102, 0, 51)
 GREEN = (51,102,0)
 BROWN = (80,26,26)
 BLACK = (0,0,0)
@@ -21,14 +46,8 @@ while gameLoop:
 
     window.fill(BROWN)
 
-    positionList = [100, 100, 70, 70]
-    for position in range(1,65):
-        pygame.draw.rect(window, GREEN, positionList)
-        if position !=0 and position %8==0:
-            positionList[1] +=72
-            positionList[0] = 100
-        else :
-            positionList[0] +=72
+    colour= statusColour()
+    draw_Field(colour)
 
     pygame.display.flip()
 pygame.quit()
