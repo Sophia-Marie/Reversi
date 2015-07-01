93c9ac609cdde1f9cc1ee5bb0ad0d369baf26de8
Sophia-Marie <soffi-marie@hotmail.com>
2015-07-01 03:17:11 +0200

    mouseposition added

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import pygame
 
-def draw_Field(colour):
-    colour= colour
+def check_mouse_position(allPositionsRect):
+    mousex,mousey = pygame.mouse.get_pos()
+    for i, positionList in enumerate(allPositionsRect):
+            return i
+        
+        
+
+def calculate_position():
     positionList = [100, 100, 70, 70]
+    allPositions=[list(positionList)]
     for position in range(1,65):
-        pygame.draw.rect(window, colour, positionList)
         if position !=0 and position %8==0:
             positionList[1] +=72
             positionList[0] = 100
-        else :
+        else:
             positionList[0] +=72
+        allPositions.append(list(positionList))
+    return allPositions
 
 
-def statusColour():
-    statusRect= pygame.mouse.get_pressed()[0]
-    statusColour=(0,0,0)
+def draw_Field(window, allPositionsRect):
+    for positionList in allPositionsRect:
+        pygame.draw.rect(window, GREEN, positionList)
     
-    if statusRect==True:
-        statusColour= PINK
-        
-    else :
-        statusColour= GREEN
-
-    return statusColour
-
 def draw_buttons():
     mousePosition= pygame.mouse.get_pos()
     clicked= pygame.mouse.get_pressed()[0]
@@ -56,10 +56,11 @@ while gameLoop:
             gameLoop=False
 
     window.fill(BROWN)
-
-    colour= statusColour()
-    draw_Field(colour)
     draw_buttons()
+    allPositionsRect = calculate_position()
+    clicked=check_mouse_position(allPositionsRect)
+    print clicked
+    draw_Field(window, allPositionsRect)
 
     pygame.display.flip()
 pygame.quit()
