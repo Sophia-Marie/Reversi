652b306de40f41abae8effa2cbc31325f295cb5c
Sophia-Marie <soffi-marie@hotmail.com>
2015-11-25 09:31:53 +0100

    added ki ON/Off but player change doesnt work

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ BLACK = (0,0,0)
 WHITE = (255,255,255)
 GREY  = (160,160,160)
 ORANGE= (255,128,0)
-
+DARKGREEN= (0,102,51)
 DEBUG = False
+#DEBUG = True
 
 def asciiPrintGameField(stoneSet):
     for i, s in enumerate(stoneSet):
@@ -169,6 +170,10 @@ def available_Moves_horizontal_flip(stoneSet, player, p, direction, foundOpponen
     
 def analyse(stoneSet, direction, player, positionArrow, doFlip):
     if DEBUG:    print "--- analysis start --- PLAYER: {}".format("BLACK" if player == 1 else "WHITE")
+        if not doFlip:
+            print "analyse only"
+        else:
+            print "analyse and flip"
     flipOk = available_Moves_horizontal(stoneSet, player, positionArrow, direction)
 
     if flipOk and doFlip:
@@ -219,21 +224,55 @@ def calc_occupied_stones(stoneSet):
 
 def draw_buttons(window, mouseX, mouseY, clicked, gameIsOver):
     newGameButtonClicked = False
-    mousePosition= pygame.mouse.get_pos()
+    font= pygame.font.Font(None, 18)
     
-    if 100 > mouseX > 50 and 70 > mouseY > 50 and clicked==1:
+    if 130 > mouseX > 50 and 70 > mouseY > 50 and clicked==1:
         pygame.draw.rect(window, GREY, (50,50,80,20))
         newGameButtonClicked = True
     elif gameIsOver:
         pygame.draw.rect(window, ORANGE, (50,50,80,20))
     else:
         pygame.draw.rect(window, WHITE, (50, 50, 80, 20))
+        
+    #player vs player button
+    if 350 > mouseX > 250 and 28 > mouseY > 8 and clicked==1:
+        pygame.draw.rect(window, DARKGREEN, (250,8,100,20))
+        playerVsPlayerClicked = True
+    else:
+        pygame.draw.rect(window, GREEN, (250,8,100,20))
+        
+    playerKiText=font.render("Player vs Player", 1, BLACK)
+    window.blit(playerKiText, (250,10,80,20))
 
-    # New Game Button
-    resetFont= pygame.font.Font(None, 16)
-    resetText= resetFont.render("New Game", 1, BLACK)
+    #player vs Ki button
+    if 475 > mouseX > 400 and 28 > mouseY > 8 and clicked==1:
+        pygame.draw.rect(window, DARKGREEN, (400,8,75,20))
+        playerVsKiClicked=True
+    else:
+        pygame.draw.rect(window, GREEN, (400,8,75,20))
+    playerKiText=font.render("Player vs Ki", 1, BLACK)
+    window.blit(playerKiText, (400,10,80,20))
+
+    # New Game button
+    resetText= font.render("New Game", 1, BLACK)
     window.blit(resetText, (60,55,50,50))
-    return newGameButtonClicked
+    
+    return newGameButtonClicked #, playerVsPlayerClicked, playerVsKiClicked
+
+def ki_on_or_off(mouseX, mouseY, clicked):
+    kiOn="Off"
+    if 350 > mouseX > 250 and 28 > mouseY > 8 and clicked==1:
+        kiOn = "Off"
+    elif 475 > mouseX > 400 and 28 > mouseY > 8 and clicked==1:
+        kiOn="On"
+
+        
+    return kiOn
+
+def ki_move_set(kiTurn):
+    if kiTurn:
+        pass
+        
 
 def blit_text_with_outline(outerText, innerText, window, position):
     window.blit(outerText, (position[0]-1,position[1],position[2],position[3]))
@@ -249,14 +288,17 @@ def blit_text_with_outline(outerText, innerText, window, position):
 def show_text(window, currentPlayer, occupiedBlack, occupiedWhite):
     font=pygame.font.Font(None, 18)
 
+    #occupied stones
     blackTextOutline=font.render("Black Stones: {}".format(occupiedBlack), 1, WHITE)
     blackText=font.render("Black Stones: {}".format(occupiedBlack), 1, BLACK)
     blit_text_with_outline(blackTextOutline, blackText, window, (640,50,20,20))
+    
 
     whiteTextOutline=font.render("White Stones: {}".format(occupiedWhite), 1, BLACK)
     whiteText=font.render("White Stones: {}".format(occupiedWhite), 1, WHITE)
     blit_text_with_outline(whiteTextOutline, whiteText, window, (640,70,20,20))
-    
+
+    #which players turn
     if currentPlayer==1:
         showPlayerText=font.render("BLACK player's turn!", 1, BLACK)
         showPlayerTextOutline=font.render("BLACK player's turn!", 1, WHITE)
@@ -264,7 +306,51 @@ def show_text(window, currentPlayer, occupiedBlack, occupiedWhite):
         showPlayerText=font.render("WHITE player's turn!", 1, WHITE)
         showPlayerTextOutline=font.render("WHITE player's turn!", 1, BLACK)
     
-    blit_text_with_outline(showPlayerTextOutline, showPlayerText, window, (350,50,200,20))        
+    blit_text_with_outline(showPlayerTextOutline, showPlayerText, window, (350,50,200,20))
+
+def player_vs_player(stoneSet, currentPlayer, positionArrow, clicked):
+    if positionArrow != None and clicked and stoneSet[positionArrow]==0:
+        flipOk = flip(stoneSet, currentPlayer, positionArrow)
+        if flipOk:
+            availableMove = any_available_move_for_next_player(stoneSet, currentPlayer)
+            if currentPlayer == 1:
+                if not availableMove:
+                    currentPlayer = 1
+                else:
+                    currentPlayer = 2
+                if DEBUG: print "current Player: {}".format("BLACK" if currentPlayer == 1 else "WHITE") 
+            else:
+                if not availableMove:
+                    currentPlayer = 2
+                else:
+                    currentPlayer = 1
+                if DEBUG: print "current Player: {}".format("BLACK" if currentPlayer == 1 else "WHITE")
+                
+def player_vs_ki(stoneSet, currentPlayer, positionArrow, clicked):
+    if currentPlayer==1:
+        if positionArrow != None and clicked and stoneSet[positionArrow]==0 and not kiTurn:
+            flipOk = flip(stoneSet, currentPlayer, positionArrow)
+            if flipOk:
+                availableMove = any_available_move_for_next_player(stoneSet, currentPlayer)
+                if currentPlayer == 1:
+                    if not availableMove:
+                        currentPlayer = 1
+                    else:
+                        currentPlayer=2
+                        kiTurn=True
+    else:
+        kiTurn=True
+        #kiMove= #funktion die den gewaehlten Stein zurueck gibt
+        flipOk=flip(stoneSet,currentplayer, kiMove)
+        if flipOk:
+            availableMove = any_available_move_for_next_player(stoneSet, currentPlayer)
+            if currentPlayer==2:
+                if not availableMove:
+                    currentPlayer = 2
+                    kiTurn=True
+                else:
+                    currentPlayer = 1
+
 
 def show_winner(window, occupiedWhite, occupiedBlack):
     window.fill((64,64,64,128), special_flags=pygame.BLEND_MULT)
@@ -298,7 +384,7 @@ def main():
     allPositionsRect = calculate_position()
     stoneSet = stones_set()
 
-    #Grundaufstellung
+    #basic lineup
     stoneSet = basic_lineup(stoneSet)
 
     gameLoop=True
@@ -310,6 +396,7 @@ def main():
                 
         mouseX, mouseY = get_mouse_position()
         clicked = check_mouse_pressed()
+        kiON =ki_on_or_off(mouseX, mouseY, clicked)
 
     ##    if not init and not clicked:
     ##        sleep(0.01)
@@ -318,21 +405,14 @@ def main():
         window.fill(BROWN)
 
         positionArrow = check_mouse_position(allPositionsRect, mouseX, mouseY)
-
-        if positionArrow != None and clicked and stoneSet[positionArrow]==0:
-            flipOk = flip(stoneSet, currentPlayer, positionArrow)
-            if flipOk:
-                availableMove = any_available_move_for_next_player(stoneSet, currentPlayer)
-                if currentPlayer == 1:
-                    if not availableMove:
-                        currentPlayer = 1
-                    else:
-                        currentPlayer=2
-                else:
-                    if not availableMove:
-                        currentPlayer = 2
-                    else:
-                        currentPlayer = 1 
+        
+##        if not kiON=="Off":
+##            pass
+##            player_vs_ki(stoneSet, currentPlayer, positionArrow, clicked)
+##            
+##        else:
+        player_vs_player(stoneSet, currentPlayer, positionArrow, clicked)
+             
 
         draw_Field(window, currentPlayer, allPositionsRect, positionArrow, stoneSet, clicked)
 
@@ -417,6 +497,12 @@ class TestPlacement(unittest.TestCase):
         flip(self.stoneSet, 1, 0)
         self.assertListEqual(expectedStoneSet, self.stoneSet)
 
+    def test_player_change(self):
+        self.stoneSet = basic_lineup(self.stoneSet)
+        currentPlayer=1
+        player_vs_player(self.stoneSet, currentPlayer, 29, 1)
+        self.assertTrue(currentPlayer==2)
+
     def test_any_available_move(self):
         availableMove = any_available_move_for_next_player(self.stoneSet, 1)
         self.assertFalse(availableMove)
@@ -424,6 +510,10 @@ class TestPlacement(unittest.TestCase):
         self.stoneSet = basic_lineup(self.stoneSet)
         availableMove = any_available_move_for_next_player(self.stoneSet, 1)
         self.assertTrue(availableMove)
+        
+    def test_kiOn(self):
+        kiOn=ki_on_or_off(300, 20, 0)
+        self.assertTrue(kiOn=="Off")
 
     def test_bug_situation_1(self):
         # Up
@@ -478,8 +568,8 @@ class TestPlacement(unittest.TestCase):
         self.assertListEqual(expectedStoneSet, self.stoneSet)
 
 if __name__ == '__main__':
-    TESTING = False
-    #TESTING = True
+    #TESTING = False
+    TESTING = True
 
     if TESTING:
         unittest.main()
